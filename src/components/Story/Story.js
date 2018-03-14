fe1feb9081c71f7ebd232d2c39728dd0f48c5c2b
Zac Cericola <zcericola@gmail.com>
2018-03-14 16:44:54 -0500

    worked on mobile responsiveness, still need to do cart

--- src/components/Story/Story.js
+++ src/components/Story/Story.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Story = () => {  
 return(<div className = 'story-container'>
- <img src = {require('../../assets/products/china.jpg')} alt = 'China' className = 'story-pic'/>    
+
     <section className = 'story-text'>
     <h1>Our Story</h1>   
     <p>Artifact Fine Goods began as all good businesses do: with a spark of inspiration and a dream.</p>
@@ -10,6 +10,7 @@ return(<div className = 'story-container'>
     <p>We were in awe of the quality of the silk available still today, and disappointed that goods of this quality weren't more widespread in America. That's when we decided to search the world and bring unique, artisan crafted goods to the masses. </p>
     <p>Finding the right pieces for sale is a time consuming and difficult process. To uphold our raison d'etre,we often only sell one of each carefully chosen item. As a result, inventory changes frequently, so check back often!</p>
     </section> 
+     <img src = {require('../../assets/products/china.jpg')} alt = 'China' className = 'story-pic'/>    
     </div>)
 }
 
